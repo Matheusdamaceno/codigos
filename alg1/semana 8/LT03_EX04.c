ed2553d16208c97ed7eead1605e8339305e3ecd0
Matheus Damaceno <damacenomartins87@gmail.com>
2023-03-23 22:34:46 -0300

    sejabemvindo

--- alg1/semana 8/LT03_EX04.c
+++ alg1/semana 8/LT03_EX04.c
@@ -1,14 +1,27 @@
-//LT03_EX04: Faça um programa em Linguagem C que receba um número do usuário e 
-//apresente a fase "Seja bem vindo", tantas vezes quanto for esse número. Uma frase por linha./
+// LT03_EX04: Faça um programa em Linguagem C que receba um número do usuário e
+// apresente a fase "Seja bem vindo", tantas vezes quanto for esse número. Uma frase por linha./
 
 #include <stdio.h>
 
+// int main()
+// { int num, cont = 0;
+//     printf("Digite um numero: "); scanf("%d", &num);
+//     while(cont < num){
+//     printf("Seja bem-vindo!\n");
+//     cont++;
+//     }
+//     return 0;
+// }
+
 int main()
-{ int num, cont = 0;
-    printf("Digite um numero: "); scanf("%d", &num);
-    while(cont < num){
-    printf("Seja bem-vindo!\n");
-    cont++;
+{
+    int num;
+    printf("Digite um numero ai ");
+    scanf("%d", &num);
+    for (int i = 0; i < num; i++)
+    {
+        printf("\nSeja bem vindo!");
     }
+
     return 0;
 }
